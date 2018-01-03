abf535002ca3574294e57b83401dfcdf6eaf4ed1
danc91 <dann.close@me.com>
2018-01-03 00:07:15 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import buildCalendar from './buildCalendar'
 const schDataEl = document.getElementById('schData')
 
 if (!schDataEl) {
-  window.alert('Please run this script on myPage Time Overview.')
+  window.alert('Please run this script on  myPage Time Overview.')
 } else {
   try {
     const schData = JSON.parse(schDataEl.value)
